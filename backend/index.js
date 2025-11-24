a727b2adbafc2893f4347cdfb892308f8fc62b5d
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-24 12:42:06 +0530

    Update Dockerfile

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const rootRouter = require("./routes/index")
 
 const app = express(); 
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
@@ -11,8 +12,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
-
+const PORT = process.env.PORT || 5000;
 app.use("/api/v1",rootRouter);
 app.listen(3000, function(req,res){
-    console.log('server is running on 3000');
+    console.log('server is running on ' + PORT);
 }); 
